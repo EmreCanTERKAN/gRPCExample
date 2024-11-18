9444bed24a7995f88c92b2d11beb4f5b4f59443e
EmreCanTERKAN <emrecanterkan06@gmail.com>
2024-11-18 10:59:26 +0300

    Add project files.

---
+++ grpcClient/Program.cs
@@ -0,0 +1,25 @@
+﻿using Grpc.Net.Client;
+using grpcServer;
+
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        // Kanalı oluşturun (gRPC sunucusuna bağlanmak için)
+        using var channel = GrpcChannel.ForAddress("https://localhost:7109");
+
+        // GreeterClient'ı başlatın
+        var greetClient = new Greeter.GreeterClient(channel);
+
+        HelloReply result = await greetClient.SayHelloAsync(new HelloRequest
+        {
+            Name = "Emreden Selamlar"
+        });
+
+        System.Console.WriteLine(result.Message);
+
+
+
+    }
+}
